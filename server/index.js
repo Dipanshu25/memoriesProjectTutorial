e94babe66c78429d0f3bee1f699d780df4e81d69
Dipanshu Tomar <dipanshutomar@Dipanshus-MacBook-Air.local>
2022-07-25 12:47:19 +0530

    Developmentbegins

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+const CONNECTION_URL =
+  "mongodb+srv://jsbegin:702638@cluster0.ft6mj.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5001;
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set("useFindAndModify", false);

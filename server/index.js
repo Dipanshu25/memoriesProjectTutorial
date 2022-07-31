9ebffd6c5f6a25c9c73718c4ffcc7e676b0333ef
Dipanshu Tomar <dipanshutomar@Dipanshus-MacBook-Air.local>
2022-07-31 17:41:43 +0530

    final

--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-app.use("/posts", postRoutes);
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
+
 const CONNECTION_URL =
-  "mongodb+srv://jsbegin:702638@cluster0.ft6mj.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://jsbegin:13985781@cluster0.ft6mj.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5001;
+
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
@@ -20,5 +25,3 @@ mongoose
     )
   )
   .catch((error) => console.log(`${error} did not connect`));
-
-//mongoose.set("useFindAndModify", false);
